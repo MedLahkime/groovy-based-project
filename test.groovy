3690668115114420876d118e58fb6cc69f93cb74
MedLahkime <38785080+MedLahkime@users.noreply.github.com>
2020-05-08 04:28:02 +0000

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,10 +1,4 @@
 def sayHello(string user){
   println 'Hello ' + user
 }
-def sayHello2(){
-  println 'hello2'
-}
-return [
-    sayHello: this.&sayHello,
-    sayHello2: this.&sayHello2
-]
+return this
